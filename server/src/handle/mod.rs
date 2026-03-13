350c6317c430a1cd3b1595a569687f25f213d8f8
hzb-macmini <3218801575@qq.com>
2026-03-13 11:23:28 +0800

    调试写一半

--- server/src/handle/mod.rs
+++ server/src/handle/mod.rs
@@ -1,3 +1,7 @@
 pub async fn health() -> &'static str {
+    let a = 1 + 2;
+    println!("a: {}", a);
+    println!("health check end");
+    println!("111");
     "ok"
 }
--- server/src/init/mod.rs
+++ server/src/init/mod.rs
@@ -1,6 +1,5 @@
 pub mod server;
 
 pub async fn init_system() {
-    println!("System initialization complete.");
     server::init_server().await;
 }
